bdf207057377b01e6a57a9c36ae371b80f71d2d6
Manikanta <manikantasai413.ms@gmail.com>
2022-08-05 23:33:25 +0530

    Added components and services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EmployeeComponent } from './components/employee/employee.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CommonComponent } from './services/common/common.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'ems-tool',
+    component: HomeComponent,
+    children: [
+      //Admin components
+      // { path: 'admindashboard', component: AdmindashboardComponent },
+      { path: 'employee/list', component: EmployeeComponent },
+      // { path: 'employee/list/:id', component: EmployComponent },
+      // { path: 'departments/list', component: DepartmentsComponent },
+      // { path: 'departments/list/:id', component: DepartmentComponent },
+      // { path: 'projects/list', component: ProjectsComponent },
+      // { path: 'projects/list/:id', component: ProjectComponent },
+      // { path: 'on-bench-employee/list', component: OnbenchComponent },
+
+      // { path: 'attendance', component: AttendanceComponent },
+      // { path: 'hrdashboard', component: HrdashboardComponent },
+      // { path: 'employeedashboard', component: EmployeedashboardComponent },
+      // { path: 'managerdashboard', component: ManagerdashboardComponent },
+      // { path: 'holidays', component: HolidaysComponent },
+      // { path: 'rating', component: RatingComponent },
+      // { path: 'leaves', component: LeavesComponent },
+    ],
+  },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const myRoutes = [
+  LoginComponent,
+  EmployeeComponent,
+  HomeComponent,
+  PageNotFoundComponent
+
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { myRoutes } from './app-routing.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    myRoutes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
